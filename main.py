b0ebfacf27e7f60476e6198d575faf5d3ab0410f
Holic <109516527+HpilOsit@users.noreply.github.com>
2024-07-09 17:05:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import logging
 import telegram
 import tempfile
 from environs import Env
-from datetime import datetime, timezone
+from datetime import *
 from time import sleep
 import asyncio
-
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
                     level=logging.INFO)
@@ -23,8 +22,8 @@ INTERVAL = env.int("INTERVAL", 300)
 COMPRESSION = env.bool("COMPRESSION", True)
 
 LATEST_NOTE_TIME = datetime.now().replace(tzinfo=timezone.utc)
-bot = telegram.Bot(token=TOKEN)
 
+bot = telegram.Bot(token=TOKEN)
 
 async def get_medias(files, temp_dir, spoiler):
     medias = []
@@ -39,12 +38,12 @@ async def get_medias(files, temp_dir, spoiler):
         elif file["type"].startswith("video"):
             try:
                 transfered = await utils.transfer_video(file["url"], temp_dir, COMPRESSION)
-            except Exception as e:
-                logging.error(f"Failed to transfer video. Falling back... {e}")
+            except:
+                logging.error("Failed to transfer video. Falling back...")
                 medias.append(
-                    telegram.InputMediaVideo(
-                        file["url"],
-                        has_spoiler=spoiler,
+                telegram.InputMediaVideo(
+                    file["url"],
+                    has_spoiler=spoiler,
                     )
                 )
                 continue
@@ -64,7 +63,6 @@ async def get_medias(files, temp_dir, spoiler):
                 )
             )
     return medias
-
 
 async def forward_new_notes(bot: telegram.Bot):
     global LATEST_NOTE_TIME
@@ -100,7 +98,7 @@ async def forward_new_notes(bot: telegram.Bot):
                             break
                         except asyncio.TimeoutError:
                             logging.warning(
-                                f"Upload media group timeout! Retrying for {i+1} time(s).")
+                                f"Upload media group timeout! Retrying for {i + 1} time(s).")
                             if i == 2:
                                 logging.error(
                                     f"Failed to upload media! Content: {n.text[:10]}... with {len(medias)} medias")
@@ -109,14 +107,12 @@ async def forward_new_notes(bot: telegram.Bot):
                         logging.info(
                             f"Forwarded a note with media, content: {n.text[:10]}... with {len(medias)} medias")
 
-
 async def main():
     logging.info("----- Bot started -----")
     while True:
         await forward_new_notes(bot)
         sleep(INTERVAL)
     logging.info("----- Bot stopped -----")
-
 
 if __name__ == "__main__":
     asyncio.run(main())
